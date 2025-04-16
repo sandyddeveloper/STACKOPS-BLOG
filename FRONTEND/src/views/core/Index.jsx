caeeea6566757c6088675d5ae991a546c1fffe31
sandyddeveloper <sandyddeveloper@gmail.com>
2025-04-17 01:00:46 +0530

    added blog section

--- FRONTEND/src/views/core/Index.jsx
+++ FRONTEND/src/views/core/Index.jsx
@@ -1,26 +1,29 @@
-import React, { useRef } from 'react'
-import MainNavbar from '../../components/main/MainNavbar'
-import CategorySection from '../../components/ui/CategorySection'
-import CategoryTicker from '../../components/ui/CategoryScroll';
-import ContentCardList from '../../components/main/ContentCardList';
+import React, { useRef } from "react";
+import MainNavbar from "../../components/main/MainNavbar";
+import CategorySection from "../../components/ui/CategorySection";
+import CategoryTicker from "../../components/ui/CategoryScroll";
+import ContentCardList from "../../components/main/ContentCardList";
+import BlogList from "../../components/main/BlogCard";
 
 const Index = () => {
   const scrollRef = useRef(null);
 
   return (
-    <div className='bg-[#dde7f5]'>
+    <div className="bg-[#dde7f5]">
       <MainNavbar />
       <CategorySection ref={scrollRef} />
       <CategoryTicker />
       <div className="flex items-center justify-center p-4 sm:p-12 w-full">
-  <div className="w-full max-w-8xl">
-    <ContentCardList />
-  </div>
-</div>
+        <div className="w-full max-w-8xl">
+          <ContentCardList />
+        </div>
+      </div>
+      <div className="px-10">
+      <BlogList />
 
-      
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Index;
